88ceedf42f88c196eb0f5d1a3b550ff894efc210
a7mad306 <103373152+a7mad306@users.noreply.github.com>
2022-04-16 02:19:12 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,50 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>Quick Links</h3>
+         <a href="home.php">HOME</a>
+         <a href="about.php">ABOUT</a>
+         <a href="shop.php">SHOP</a>
+         <a href="contact.php">CONTACT</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra Links</h3>
+         <a href="login.php">LOGIN</a>
+         <a href="register.php">REGISTER</a>
+         <a href="cart.php">CART</a>
+         <a href="orders.php">ORDERS</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact Info</h3>
+         <p> <i class="fas fa-phone"></i> 07503069936 </p>
+         <p> <i class="fas fa-phone"></i> 07503069936 </p>
+         <p> <i class="fas fa-envelope"></i> a7mad.sekany306@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Bardarash-Duhok-Kurdistan </p>
+      </div>
+
+      <div class="box">
+         <h3>Follow Us</h3>
+         <a href="https://www.facebook.com/hahahaha.drm.deshy.drrr"> <i class="fab fa-facebook-f"></i> Facebook </a>
+         <a href="https://www.snapchat.com/add/a7mau306?share_id=QTM3MDIz&locale=en_IQ"> <i class="fab fa-snapchat"></i> Snapchat </a>
+         <a href="https://www.instagram.com/a7mad._kurdish/?r=nametag"> <i class="fab fa-instagram"></i> Instagram </a>
+         <a href="https://t.me/a7mau306"> <i class="fab fa-telegram"></i> Telegram </a>
+      </div>
+
+   </div>
+<div class="box">
+   <h1>Prepared By</h1>
+   <h2> <i class="fas fa-user"></i> Ahmad Abdulsamad</h2>
+   <h2> <i class="fas fa-user"></i> Manal Asem</h2>
+   <h2> <i class="fas fa-user"></i> Narjs Jalal</h2>
+</div>
+<div align="center">
+   <h1>Lecturer</h1>
+   <h2> <i class="fas fa-user"></i> Mr.Rakan S.Rashid</h2>
+</div>
+   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span>Ahmad Abdulsamad </span> </p>
+
+</section>
